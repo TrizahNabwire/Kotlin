926a5bcb90757f61725ecce1e6ec8cd696940124
Trizah Nabwire <99127888+TrizahNabwire@users.noreply.github.com>
2022-07-07 08:05:35 -0700

    Add files via upload
    Printing a Birthday Cake

--- birthday.kt
+++ birthday.kt
@@ -1,15 +1,38 @@
-
 fun main(){
-    val border= "|"
-    val timesToRepeat = 25
-    printBorder(border, timesToRepeat)
-    println("HAPPY BIRTHDAY TRIZAH")
-    printBorder(border, timesToRepeat)
-}
-fun printBorder(border: String, timesToRepeat: Int){
-        repeat(timesToRepeat){
-            print(border)
+    val age = 19
+    val layers = 7
+    printCakeCandles(age)
+    printCakeTop(age)
+    printCakeBottom(age, layers)
 
+}
+
+
+fun printCakeCandles(age: Int) {
+    repeat(age) {
+        print(",")
+    }
+    println()
+
+    print("")
+    repeat(age) {
+        print("|")
+    }
+    println("")
+}
+
+fun printCakeTop(age: Int) {
+    print("")
+    repeat(age + 4) {
+        print("=")
     }
     println()
 }
+
+fun printCakeBottom(age: Int, layers: Int){
+    repeat(age + 2){
+        print("@")
+    }
+    println("")
+}
+
